4dbafe11a772a368d43f50de716fd0d6b1e4943b
Eraldo <eraldosimaopereira@gmail.com>
2023-06-27 19:39:09 -0300

    removido key

--- app/desktop/src/view/main/MainView.js
+++ app/desktop/src/view/main/MainView.js
@@ -1,68 +1,68 @@
 Ext.define("ModernApp.view.main.MainView", {
-	extend: "Ext.Container",
-	xtype: "mainview",
-	cls: "mainview",
-	controller: "mainviewcontroller",
-	viewModel: {
-		type: "mainviewmodel",
-	},
-	layout: {
-		type: "vbox",
-		align: "start",
-	},
-	items: [
-		{
-			xtype: "container",
-			cls: "header",
-			layout: {
-				type: "hbox",
-				align: "center",
-			},
-			items: [
-				{
-					xtype: "image",
-					cls: "logo",
-					src: "https://ncdn0.infojobs.com.br/logos/2015/01/18/354178.jpg",
-				},
-				{
-					xtype: "component",
-					cls: "title",
-					html: "Movies",
-				},
-				{
-					xtype: "component",
-					cls: "title",
-					html: "Series",
-				},
-			],
-		},
-		{
-			xtype: "container",
-			flex: 1,
-			layout: {
-				type: "hbox",
-				align: "stretch",
-			},
-			items: [
+  extend: "Ext.Container",
+  xtype: "mainview",
+  cls: "mainview",
+  controller: "mainviewcontroller",
+  viewModel: {
+    type: "mainviewmodel",
+  },
+  layout: {
+    type: "vbox",
+    align: "start",
+  },
+  items: [
+    {
+      xtype: "container",
+      cls: "header",
+      layout: {
+        type: "hbox",
+        align: "center",
+      },
+      items: [
+        {
+          xtype: "image",
+          cls: "logo",
+          src: "https://ncdn0.infojobs.com.br/logos/2015/01/18/354178.jpg",
+        },
+        {
+          xtype: "component",
+          cls: "title",
+          html: "Movies",
+        },
+        {
+          xtype: "component",
+          cls: "title",
+          html: "Series",
+        },
+      ],
+    },
+    {
+      xtype: "container",
+      flex: 1,
+      layout: {
+        type: "hbox",
+        align: "stretch",
+      },
+      items: [
         {
           xtype: "filters",
           // height: 600,
-				},
-				{
+        },
+        {
           xtype: "container",
           flex: 1,
-          scrollable: 'y',
-					layout: {
+          scrollable: "y",
+          layout: {
             type: "vbox",
-						align: "stretch",
+            align: "stretch",
             wrap: true,
-					},
-					items: {
-						xtype: "movie",
-            scrollable: 'y'
-					},
-				},
-			],
-		},
-	],
+          },
+          items: {
+            xtype: "movie",
+            scrollable: "y",
+          },
+        },
+      ],
+    },
+  ],
 });
